e96540efaa39ad03f3dd3cc83e660d7de6c4029a
Rafael <rafael-dev@hotmail.com>
2024-07-29 22:06:54 -0300

    feat: get_all_tx

--- src/modules/transaction/service.rs
+++ src/modules/transaction/service.rs
@@ -1,12 +1,16 @@
-use std::clone;
-
 use ::redis::Commands;
 use axum::http::Error;
 use redis::RedisResult;
+use serde::Serialize;
 
-use crate::consumers::transaction;
 use crate::database::redis::redis_client;
 use crate::types::transaction::types::Transaction;
+
+#[derive(Serialize)]
+pub struct GetAllTxReturn {
+    pub transactions: Vec<Transaction>,
+    pub count: usize,
+}
 
 pub fn create_transaction(payload: Transaction) {
     let mut db = redis_client();
@@ -26,16 +30,21 @@ pub fn create_transaction(payload: Transaction) {
     };
 }
 
-pub fn get_user_transactions(id: String) -> Result<Vec<Transaction>, Error> {
+pub fn get_all_transactions() -> Result<GetAllTxReturn, Error> {
     let mut db = redis_client();
-    let tx_list = format!("{}{}", "transactions:".to_owned(), id);
+    let formatted_tx_key = format!("{}{}", "transaction:".to_owned(), '*');
 
-    let res: Vec<String> = db.lrange(tx_list, 0, -1).unwrap();
+    let res: Vec<String> = db.keys(formatted_tx_key).unwrap();
 
     let mut transactions: Vec<Transaction> = Vec::new();
 
+    let count = res.len();
+
     if res.len() == 0 {
-        Ok(transactions.clone())
+        Ok(GetAllTxReturn {
+            transactions: transactions.clone(),
+            count,
+        })
     } else {
         for key in res {
             let transaction: RedisResult<String> = db.get(key);
@@ -52,30 +61,60 @@ pub fn get_user_transactions(id: String) -> Result<Vec<Transaction>, Error> {
             }
         }
 
-        Ok(transactions.clone())
+        Ok(GetAllTxReturn {
+            transactions: transactions.clone(),
+            count,
+        })
+    }
+}
+
+pub fn get_user_transactions(id: String) -> Result<GetAllTxReturn, Error> {
+    let mut db = redis_client();
+    let tx_list = format!("{}{}", "transactions:".to_owned(), id);
+
+    let res: Vec<String> = db.lrange(tx_list, 0, -1).unwrap();
+
+    let mut transactions: Vec<Transaction> = Vec::new();
+
+    let count = res.len();
+
+    if res.len() == 0 {
+        Ok(GetAllTxReturn {
+            transactions: transactions.clone(),
+            count,
+        })
+    } else {
+        for key in res {
+            let formatted_tx_id = format!("{}{}", "transaction:".to_owned(), key);
+            let transaction: RedisResult<String> = db.get(formatted_tx_id);
+
+            match transaction {
+                Ok(transaction) => {
+                    let parsed_transaction: Transaction =
+                        serde_json::from_str(transaction.as_str()).expect("error");
+
+                    transactions.push(parsed_transaction)
+                }
+
+                Err(_) => {}
+            }
+        }
+
+        Ok(GetAllTxReturn {
+            transactions: transactions.clone(),
+            count,
+        })
     }
 }
 
 pub fn get_transaction_by_id(id: String) -> Result<Transaction, serde_json::Error> {
     let mut db = redis_client();
+    let formatted_tx_id = format!("{}{}", "transaction:".to_owned(), id);
 
-    let transaction: String = db.get(id).unwrap();
+    let transaction: String = db.get(formatted_tx_id).unwrap();
 
     let parsed_transaction: Result<Transaction, serde_json::Error> =
         serde_json::from_str(&transaction.as_str());
 
     parsed_transaction
 }
-
-// pub fn get_transaction_by_id(transaction_id: String, user_id: String) {
-//     let mut db = redis_client();
-
-//     let mut iter: redis::Iter<isize> = redis::cmd("SSCAN")
-//         .arg(user_id)
-//         .cursor_arg(0)
-//         .clone()
-//         .iter(&mut db)
-//         .unwrap();
-
-//     println!("Iter Result: {:?}", iter)
-// }
