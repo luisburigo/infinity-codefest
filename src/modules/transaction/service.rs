6362a1bc025958cca9465e9e1b463dea70d2d7dc
luisburigo <guga.burigo29@outlook.com>
2024-07-29 22:48:30 -0300

    feat: check sender and receiver

--- src/modules/transaction/service.rs
+++ src/modules/transaction/service.rs
@@ -20,66 +20,75 @@ pub fn create_transaction(mut payload: Transaction) -> Result<Transaction, Redis
     let sender_id = payload.sender.unwrap().to_string();
     let receiver_id = payload.receiver.unwrap().to_string();
 
-    let mut sender = match get_user(payload.sender.unwrap()) {
-        Ok(value) => User::from(value),
-        Err(e) => return Err(e.into())
+    let mut sender: Result<User, RedisError> = match get_user(payload.sender.unwrap()) {
+        Ok(value) => Ok(User::from(value)),
+        Err(e) => Err(e.into())
     };
-    let mut receiver = match get_user(payload.receiver.unwrap()) {
-        Ok(value) => User::from(value),
-        Err(e) => return Err(e.into())
+    let mut receiver: Result<User, RedisError> = match get_user(payload.receiver.unwrap()) {
+        Ok(value) => Ok(User::from(value)),
+        Err(e) => Err(e.into())
     };
 
-    // Check sender has balance to send
-    let sender_balance = sender.balance.unwrap();
-    let sender_currency = sender.currency.clone().unwrap();
-    let sender_balance_usd = sender_currency.to_usd(sender_balance);
+    // Check if has sender and receiver
+    if sender.is_ok() && receiver.is_ok() {
+        let mut sender = sender.unwrap();
+        let mut receiver = receiver.unwrap();
 
-    let transaction_amount = payload.amount;
-    let transaction_currency = payload.currency.clone().unwrap();
-    let transaction_amount_usd = transaction_currency.to_usd(transaction_amount);
-    let has_balance = sender_balance_usd >= transaction_amount_usd;
+        // Check sender has balance to send
+        let sender_balance = sender.balance.unwrap();
+        let sender_currency = sender.currency.clone().unwrap();
+        let sender_balance_usd = sender_currency.to_usd(sender_balance);
 
-    // eprintln!("\n----------------------");
-    // eprintln!("Sending transaction:");
-    // eprintln!("TX Currency: {:?}", transaction_currency);
-    // eprintln!("TX Amount: {:?}", transaction_amount);
-    // eprintln!("TX Amount USD: {:?}", transaction_amount_usd);
-    // eprintln!("Sender Currency: {:?}", sender_currency);
-    // eprintln!("Sender Balance: {:?}", sender_balance);
-    // eprintln!("Sender Balance USD: {:?}", sender_balance_usd);
+        let transaction_amount = payload.amount;
+        let transaction_currency = payload.currency.clone().unwrap();
+        let transaction_amount_usd = transaction_currency.to_usd(transaction_amount);
+        let has_balance = sender_balance_usd >= transaction_amount_usd;
 
-    match has_balance {
-        true => {
-            // Update sender balance
-            let sender_balance_usd = sender_balance_usd - transaction_amount_usd;
-            sender.balance = Some(sender_currency.from_usd(sender_balance_usd));
-            create_user(sender.clone());
-            // eprintln!("Sender New Balance: {:?}", sender.balance);
+        // eprintln!("\n----------------------");
+        // eprintln!("Sending transaction:");
+        // eprintln!("TX Currency: {:?}", transaction_currency);
+        // eprintln!("TX Amount: {:?}", transaction_amount);
+        // eprintln!("TX Amount USD: {:?}", transaction_amount_usd);
+        // eprintln!("Sender Currency: {:?}", sender_currency);
+        // eprintln!("Sender Balance: {:?}", sender_balance);
+        // eprintln!("Sender Balance USD: {:?}", sender_balance_usd);
 
-            // Update receiver balance
-            let receiver_balance = receiver.balance.unwrap();
-            let receiver_currency = receiver.clone().currency.unwrap();
-            let receiver_balance_usd = receiver_currency.to_usd(receiver_balance);
+        match has_balance {
+            true => {
+                // Update sender balance
+                let sender_balance_usd = sender_balance_usd - transaction_amount_usd;
+                sender.balance = Some(sender_currency.from_usd(sender_balance_usd));
+                create_user(sender.clone());
+                // eprintln!("Sender New Balance: {:?}", sender.balance);
 
-            let receiver_balance_usd = receiver_balance_usd + transaction_amount_usd;
-            receiver.balance = Some(receiver_currency.from_usd(receiver_balance_usd));
-            create_user(receiver.clone());
+                // Update receiver balance
+                let receiver_balance = receiver.balance.unwrap();
+                let receiver_currency = receiver.clone().currency.unwrap();
+                let receiver_balance_usd = receiver_currency.to_usd(receiver_balance);
 
-            // Update transaction status
-            payload.status = Some(TransactionStatus::Approved);
+                let receiver_balance_usd = receiver_balance_usd + transaction_amount_usd;
+                receiver.balance = Some(receiver_currency.from_usd(receiver_balance_usd));
+                create_user(receiver.clone());
 
-            eprintln!("Transaction approved!");
-            // eprintln!("Receiver Currency: {:?}", receiver_currency);
-            // eprintln!("Receiver Balance: {:?}", receiver_balance);
-            // eprintln!("Receiver New Balance: {:?}", receiver.balance);
+                // Update transaction status
+                payload.status = Some(TransactionStatus::Approved);
+
+                eprintln!("Transaction approved!");
+                // eprintln!("Receiver Currency: {:?}", receiver_currency);
+                // eprintln!("Receiver Balance: {:?}", receiver_balance);
+                // eprintln!("Receiver New Balance: {:?}", receiver.balance);
+            }
+            false => {
+                payload.status = Some(TransactionStatus::Failed);
+                eprintln!("Transaction failed: Insufficient balance");
+            }
         }
-        false => {
-            payload.status = Some(TransactionStatus::Failed);
-            eprintln!("Transaction failed: Insufficient balance");
-        }
+
+        // eprintln!("----------------------\n");
+    } else {
+        payload.status = Some(TransactionStatus::Failed);
+        eprintln!("Transaction failed: Sender or receiver not found");
     }
-
-    // eprintln!("----------------------\n");
 
     let tx_id = payload.id.unwrap();
 
